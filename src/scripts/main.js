2d50f1068414278579b07732cbe4c55811ace26b
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-04-25 04:35:39 +0300

    create sort table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('thead tr th');
+
+headers.forEach((header) => {
+  header.addEventListener('click', () => {
+    const table = document.querySelector('table');
+    const rows = Array.from(table.querySelectorAll('tbody tr'));
+    const index = Array.from(header.parentNode.children).indexOf(header);
+    const sortedRows = rows.sort((a, b) => {
+      const aText = a.querySelectorAll('td')[index].textContent;
+      const bText = b.querySelectorAll('td')[index].textContent;
+
+      return aText.localeCompare(bText);
+    });
+    const tbody = table.querySelector('tbody');
+
+    tbody.innerHTML = '';
+    sortedRows.forEach((row) => tbody.appendChild(row));
+  });
+});
